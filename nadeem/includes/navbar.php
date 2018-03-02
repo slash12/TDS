d57ddfa2c22014d41499f78c0ef791c4e47deb84
slash12 <mgardasaib@umail.utm.ac.mu>
2018-03-02 15:56:17 +0400

    footer style complete, need to fill the content and make responsive

--- nadeem/includes/navbar.php
+++ nadeem/includes/navbar.php
@@ -1,18 +1,12 @@
-<!DOCTYPE html>
-<html>
-<head>
+
   <link type="text/css" rel="stylesheet" href="../css/style.css"/>
-</head>
-  <body>
     <nav>
       <div id="nav-wrapper" class="container">
-        <a href="#home" id="logo">ShirtPrints</a>
+        <a href="#"></a>
+        <a href="#home" id="logo"><img src="../img/logonav.png">ShirtPrints</a>
         <a href="#home" class="link">Browse T-Shirt</a>
         <a href="#home" class="link">Create T-Shirt</a>
-
         <a href="#Register" class="creden">Register</a>
         <a href="#Login" class="creden">Login</a>
       </div>
     </nav>
-  </body>
-</html>
