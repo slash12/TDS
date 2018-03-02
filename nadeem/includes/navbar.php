01b348960f690b5a6a3823f5ff546724c2060b88
slash12 <mgardasaib@umail.utm.ac.mu>
2018-03-02 11:56:47 +0400

    navbar layout css completed

---
+++ nadeem/includes/navbar.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link type="text/css" rel="stylesheet" href="../css/style.css"/>
+</head>
+  <body>
+    <nav>
+      <div id="nav-wrapper" class="container">
+        <a href="#home" id="logo">ShirtPrints</a>
+        <a href="#Register" class="creden">Register</a>
+        <a href="#Login" class="creden">Login</a>
+      </div>
+    </nav>
+  </body>
+</html>
