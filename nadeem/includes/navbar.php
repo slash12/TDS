c85a9691e4f176ce87d01f75307a14532f94225a
slash12 <mgardasaib@umail.utm.ac.mu>
2018-03-02 14:13:04 +0400

    Navbar model completed

--- nadeem/includes/navbar.php
+++ nadeem/includes/navbar.php
@@ -7,6 +7,9 @@
     <nav>
       <div id="nav-wrapper" class="container">
         <a href="#home" id="logo">ShirtPrints</a>
+        <a href="#home" class="link">Browse T-Shirt</a>
+        <a href="#home" class="link">Create T-Shirt</a>
+
         <a href="#Register" class="creden">Register</a>
         <a href="#Login" class="creden">Login</a>
       </div>
